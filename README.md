90e97c7bd2212442803fa2decbc2919d465338b1
Сергей Матросов <matrosov85@yandex.ru>
2023-01-08 20:02:20 +0300

    9th sprint 1st try

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# api_final
-api final
+# Проект "API для Yatube"
+
+## Описание
+
+Проект добавляет интерфейс API к проекту Yatube, благодаря чему повявляются следующие возможности:
+* Создание/просмотр/редактирование/удаление публикаций
+* Создание/просмотр/редактирование/удаление комментариев к публикациям
+* Просмотр сообществ
+* Создание/просмотр подписок на авторов
+* JWT авторизация
+
+## Как запустить проект:
+Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone git@github.com:matrosov85/api_final_yatube.git
+```
+```
+cd api_final_yatube
+```
+Cоздать и активировать виртуальное окружение:
+```
+python -m venv venv
+```
+```
+source venv/scripts/activate
+```
+Установить зависимости из файла requirements.txt:
+```
+python -m pip install --upgrade pip
+```
+```
+pip install -r requirements.txt
+```
+Выполнить миграции:
+```
+python manage.py migrate
+```
+Запустить проект:
+```
+python manage.py runserver
+```
+
+## Примеры запросов
+Подробное описание всех запросов к API http://127.0.0.1:8000/redoc/
+* #### Создание публикации
+    **Request**
+    ```
+    POST /api/v1/posts/
+    {
+        "text": "string",
+        "image": "string",
+        "group": 0
+    }
+    ```
+    **Response**
+    ```
+    {
+        "id": 0,
+        "author": "string",
+        "text": "string",
+        "pub_date": "2019-08-24T14:15:22Z",
+        "image": "string",
+        "group": 0
+    }
+    ```
+* #### Получение информации о сообществе
+    **Request**
+    ```
+    GET /api/v1/groups/{id}/
+    ```
+    **Response**
+    ```
+    {
+        "id": 0,
+        "title": "string",
+        "slug": "string",
+        "description": "string"
+    }
+    ```
+* #### Удаление комментария
+    **Request**
+    ```
+    DELETE /api/v1/posts/{post_id}/comments/{id}/
+    ```
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,57 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('id', 'author', 'text', 'pub_date', 'image', 'group')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'text', 'created', 'post')
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
     )
 
     class Meta:
-        fields = '__all__'
-        model = Comment
+        model = Follow
+        fields = ('user', 'following')
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message=('Нельзя повторно подписаться на того же автора.')
+            ),
+        )
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError('Нельзя подписаться на себя.')
+        return data
